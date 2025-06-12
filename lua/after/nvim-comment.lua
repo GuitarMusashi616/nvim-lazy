75b934ac1467bd9894fdb0bd6bc3c0cfd47a352c
Austin Williams <awilliams@akcourts.gov>
2025-06-12 08:43:15 -0800

    Add keybinds and toggle comment

--- lua/after/nvim-comment.lua
+++ lua/after/nvim-comment.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>;", ":CommentToggle<CR>")
+vim.keymap.set({"n", "v"}, "<leader>;", ":CommentToggle<CR>")
+vim.keymap.set("n", "gcc", ":CommentToggle<CR>")
+vim.keymap.set("v", "gc", ":CommentToggle<CR>")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,19 +2,23 @@
 vim.g.mapleader = " "
 
 -- save
-vim.keymap.set("n", "<leader>fs", ":w<CR>")
-vim.keymap.set("n", "<leader>fS", ":wa<CR>")
+vim.keymap.set("n", "<leader>fs", ":w<cr>")
+vim.keymap.set("n", "<leader>fS", ":wa<cr>")
 
 -- close
-vim.keymap.set("n", "<leader>c", ":q<CR>")
+vim.keymap.set("n", "<leader>x", ":q<cr>")
 
 -- quit all
-vim.keymap.set("n", "<leader>qq", ":wqa<CR>")
+vim.keymap.set("n", "<leader>qq", ":wqa<cr>")
 
--- buffer left/right
-vim.keymap.set("n", "L", ":bn<CR>")
-vim.keymap.set("n", "H", ":bp<CR>")
-vim.keymap.set("n", "<leader>bd", ":bd<CR>")
+-- buffers
+vim.keymap.set("n", "L", ":bn<cr>")
+vim.keymap.set("n", "H", ":bp<cr>")
+vim.keymap.set("n", "<leader>bd", ":bd<cr>")
+
+-- yank & paste
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 
 -- windows
 vim.keymap.set("n", "<leader>wv", "<c-w>v")
@@ -25,6 +29,9 @@ vim.keymap.set("n", "<leader>wj", "<c-w>j")
 vim.keymap.set("n", "<leader>wk", "<c-w>k")
 vim.keymap.set("n", "<leader>wd", "<c-w>d")
 
+-- misc
 vim.keymap.set("n", "<c-d>", "yyp")
+vim.keymap.set("v", "ae", "<ESC>ggVG")
+vim.keymap.set("n", "feD", ":e ~/.config/nvim/lua/keymaps.lua<cr>")
 
 
